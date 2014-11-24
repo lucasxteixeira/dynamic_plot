26b22688fde79b8b170bbfbece0e67f89f5c5767
lucasxteixeira <lucasxteixeira@gmail.com>
2014-11-24 13:35:05 -0200

    Add more examples

--- app/app.R
+++ app/app.R
@@ -1,15 +1,39 @@
 ## app.R
 
-library(RCurl)
+library(curl)
+library(rCharts)
 library(maps)
 library(leaflet)
+library(reshape2)
+
 
 
 server <- function(input, output, session) {
 
-	doc_url <- "https://docs.google.com/spreadsheets/d/1mzlXYuJ3RBdr_X94dhrHasndG1uoSD2VvjIq7nBDbaQ/export?gid=0&format=csv"
-	doc_csv <- getURL(doc_url,.opts=list(ssl.verifypeer=FALSE))
-	data <- read.csv(textConnection(doc_csv))
+	data <- read.csv(curl("https://docs.google.com/spreadsheets/d/1mzlXYuJ3RBdr_X94dhrHasndG1uoSD2VvjIq7nBDbaQ/export?gid=0&format=csv"))
+
+	output$populationBarChart <- renderChart2({
+	    nPlot(Population ~ City, data = data, type = "multiBarChart")
+	})
+
+	output$diseaseLineChart <- renderChart2({
+	    diseaseLineChart <- rPlot(Pop_with_Heart_Diseases ~ City, data = data, type = 'line')
+	    diseaseLineChart$guides(y = list(min = 0, title = "Population with Heart Diseases"))
+	    return(diseaseLineChart)
+	})
+
+	output$diseaseBarChart <- renderChart2({
+		nPlot(Obese_Population ~ Obese_Treament, group = "Country", data = data, type = "multiBarChart")
+	})
+
+	output$obeseDistributionChart <- renderChart2({
+		obesityDistribution = melt(data[-c(1,3:8)], id.vars="City")
+	    d1 = dPlot(y="City", x="value",data=obesityDistribution, groups="variable",type="bar")
+		d1$xAxis(type = "addPctAxis")
+		d1$yAxis(type = "addCategoryAxis")
+		d1$legend( x = 60, y = 10, width = 700, height = 20, horizontalAlign = "left")
+	    return(d1)
+	})
 
 	map <- createLeafletMap(session, 'map')
 
@@ -23,7 +47,7 @@ server <- function(input, output, session) {
 		   	map$addMarker(
 		        point$Latitude,
 		        point$Longitude,
-		        point$Location,
+		        point$City,
 		        list(
 		          	awesome=list(
 		            	icon=NULL, 
@@ -40,6 +64,18 @@ server <- function(input, output, session) {
 ui <- shinyUI(fluidPage(
 	titlePanel("Dynamic Plot"),
 	fluidRow(
+		h3("rCharts"),
+		h5("Population bar chart:"),
+		showOutput("populationBarChart", "nvd3"),
+		h5("Population with heart diseases line chart:"),
+		showOutput("diseaseLineChart", "polycharts"),
+		h5("Obese treatement by country bar chart:"),
+		showOutput("diseaseBarChart", "nvd3"),
+		h5("Obesity distribution dimple:"),
+		showOutput("obeseDistributionChart","dimple")
+	),
+	fluidRow(
+		h3("leaflet-shiny"),
 	   	leafletMap(
 		    "map", "100%", "700px",
 		    options=list(
